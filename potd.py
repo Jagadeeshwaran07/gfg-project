9e8e6afd0f09310b2fccd1c27ebde7911d563b92
Jagadeeshwaran M <85460652+Jagadeeshwaran07@users.noreply.github.com>
2023-09-30 17:33:20 +0530

    Commented

--- potd.py
+++ potd.py
@@ -1,24 +1,26 @@
-goal = 200
+#Code Implementation used
 
-streak = 0
+# goal = 200
 
-for8 = streak%8
-geekbits = 0
-days = 0
+# streak = 0
 
-for i in range(goal+1):
-    days +=1
-    for8 += 1
-    geekbits +=1
+# for8 = streak%8
+# geekbits = 0
+# days = 0
 
-    # print(days, for8, geekbits)
+# for i in range(goal+1):
+#     days +=1
+#     for8 += 1
+#     geekbits +=1
 
-    if for8 == 8:
-        geekbits +=8
-        for8 = 0
+#     # print(days, for8, geekbits)
 
-    if geekbits >= goal:
-        print(days)
-        break
+#     if for8 == 8:
+#         geekbits +=8
+#         for8 = 0
+
+#     if geekbits >= goal:
+#         print(days)
+#         break
     
-    # 1.1.1 2.2.2 3.3.3 4.4.4 5.5.5 6.6.6 7.7.7 8.0.16
+#     # 1.1.1 2.2.2 3.3.3 4.4.4 5.5.5 6.6.6 7.7.7 8.0.16
